a2d8f13cc7e3a0755ce9e6d8835a729eeeb57160
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2022-09-22 14:14:23 +0200

    add sql scrypts

---
+++ DDL/Tables/CreateTable-rating.sql
@@ -0,0 +1,16 @@
+USE Multisport;
+
+DROP TABLE IF EXISTS rating;
+
+CREATE TABLE rating
+(
+    user_id      INT NOT NULL,
+    sportclub_id INT NOT NULL,
+    rate         INT NOT NULL,
+    comment      VARCHAR(100),
+
+    CONSTRAINT PK_rating PRIMARY KEY
+        (
+         user_id, sportclub_id
+            )
+);
