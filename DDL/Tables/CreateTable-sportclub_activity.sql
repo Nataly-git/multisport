692fc58eb0cdc21f8e534804e5bbaae45839a1c8
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2022-09-22 14:26:09 +0200

    add sql scrypts

---
+++ DDL/Tables/CreateTable-sportclub_activity.sql
@@ -0,0 +1,16 @@
+USE
+Multisport;
+
+DROP TABLE IF EXISTS sportclub_activities;
+
+CREATE TABLE sportclub_activities
+(
+    sportclub_id INT           NOT NULL,
+    activity_id  INT           NOT NULL,
+    duration     DECIMAL(3, 2) NOT NULL,
+
+    CONSTRAINT PK_sportclub_activities PRIMARY KEY
+        (
+         sportclub_id, activity_id
+            )
+);
