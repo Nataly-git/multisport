d27beaa311410a4976ce97c0c7ea394b61ee03f3
RodionAparin <rodionaparing@coherentsolutions.com>
2022-09-23 17:25:15 +0300

    fixed sql

--- DDL/JSONParser/Procedure-usp_parse_json_predefined_card_type.sql
+++ DDL/JSONParser/Procedure-usp_parse_json_predefined_card_type.sql
@@ -1,4 +1,4 @@
-USE multisport;
+USE Multisport;
 
 DELIMITER $$
 
@@ -6,15 +6,15 @@ DROP PROCEDURE IF EXISTS usp_parse_json_predefined_card_type;
 
 CREATE PROCEDURE usp_parse_json_predefined_card_type(IN json_predefined_card_type text)
 BEGIN
+    INSERT INTO card_type(type, price, max_visit_number)
     SELECT type, price, max_visit_number
-    INTO @type, @price,@max_visit_number
     FROM json_table(json_predefined_card_type, '$[*]'
                     COLUMNS (
                         type VARCHAR(30) PATH '$.type',
                         price DECIMAL(6, 2) PATH '$.price',
                         max_visit_number TINYINT PATH '$.maxVisitsNumber'
                         )
-             ) AS card_types;
+             ) as card_types;
 
-    INSERT INTO card_type(type, price, max_visit_number) value (@type, @price, @max_visit_number);
+
 END$$
