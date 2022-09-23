0d376ac94da52e74109b577a7e194de236ff0d89
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2022-09-23 14:47:43 +0200

    fixed

--- DDL/Constraints/Constraint-order.sql
+++ DDL/Constraints/Constraint-order.sql
@@ -1,12 +1,12 @@
 USE Multisport;
 
-CALL usp_addUniqueKey('order', 'AK_order_date_user', 'date, user_id');
+CALL usp_addUniqueKey('`order`', 'AK_order_date_user', 'date, user_id');
 
-CALL usp_AddForeignKey('order', 'FK_order_user', 'user_id',
+CALL usp_AddForeignKey('`order`', 'FK_order_user', 'user_id',
                        'user', 'user_id');
 
-CALL usp_AddForeignKey('order', 'FK_order_status', 'status_id',
+CALL usp_AddForeignKey('`order`', 'FK_order_status', 'status_id',
                        'order_status', 'status_id');
 
-CALL usp_AddForeignKey('order', 'FK_order_card_type', 'card_type_id',
+CALL usp_AddForeignKey('`order`', 'FK_order_card_type', 'card_type_id',
                        'card_type', 'card_type_id');
--- DDL/Constraints/Constraint-sportclub_card_types.sql
+++ DDL/Constraints/Constraint-sportclub_card_types.sql
@@ -4,4 +4,4 @@ CALL usp_AddForeignKey('sportclub_card_types', 'FK_sportclub_card_types_sportclu
                        'sportclub', 'sportclub_id');
 
 CALL usp_AddForeignKey('sportclub_card_types', 'FK_sportclub_card_types_card_type', 'card_type_id',
-                       'card_types', 'cart_type_id');
+                       'card_type', 'card_type_id');
