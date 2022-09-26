dde2624bdb2a3021e7e4350f538cc0435f911a24
RodionAparin <rodionaparing@coherentsolutions.com>
2022-09-26 12:39:27 +0300

    fixed

---
+++ DDL/JSONParser/Procedure-usp_parse_json_create_order.sql
@@ -0,0 +1,21 @@
+USE Multisport;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS usp_parse_json_create_order;
+
+CREATE PROCEDURE usp_parse_json_create_order(IN json_create_order TEXT)
+BEGIN
+    INSERT INTO `order`(user_id, date, status_id, card_type_id)
+    SELECT user_id, `date`, status_id, card_type_id
+    FROM json_table(json_create_order, '$'
+                    COLUMNS (
+                        user_email VARCHAR(254) PATH '$.user.email',
+                        `date` DATE PATH '$.order.date',
+                        order_status VARCHAR(20) PATH '$.order.orderStatus.status',
+                        card_type VARCHAR(30) PATH '$.cardType.type'
+                        )) as card_order
+             left join user on card_order.user_email = user.email
+             left join order_status on card_order.order_status = order_status.status
+             left join card_type on card_order.card_type = card_type.type;
+END$$
