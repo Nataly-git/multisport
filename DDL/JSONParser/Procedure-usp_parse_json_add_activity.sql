a02e25b6f2df94b139e31fb378113f1d1ceaa055
RodionAparin <rodionaparing@coherentsolutions.com>
2022-09-26 11:59:12 +0300

    fixed

---
+++ DDL/JSONParser/Procedure-usp_parse_json_add_activity.sql
@@ -0,0 +1,20 @@
+USE Multisport;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS usp_parse_json_add_activity;
+
+CREATE PROCEDURE usp_parse_json_add_activity(IN json_add_activity TEXT)
+BEGIN
+    INSERT INTO sportclub_activity(sportclub_id, activity_id, duration)
+    SELECT sportclub_id, activity_id, duration
+    FROM json_table(json_add_activity, '$'
+                    COLUMNS (
+                        sportclubEmail VARCHAR(254) PATH '$.sportClub.email',
+                        activityName VARCHAR(30) PATH '$.activity.name',
+                        duration DECIMAL(3, 2) PATH '$.duration'
+                        )) as add_activity
+             left join sportclub on add_activity.sportclubEmail = sportclub.email
+             left join activity on add_activity.activityName = activity.name;
+END
+$$
