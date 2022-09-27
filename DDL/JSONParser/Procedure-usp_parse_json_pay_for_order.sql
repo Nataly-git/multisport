6ffa25d70df0efebfd1fad7da3062df2c893e67a
RodionAparin <rodionaparing@coherentsolutions.com>
2022-09-27 10:58:51 +0300

    fixed payment

--- DDL/JSONParser/Procedure-usp_parse_json_pay_for_order.sql
+++ DDL/JSONParser/Procedure-usp_parse_json_pay_for_order.sql
@@ -10,9 +10,9 @@ BEGIN
     INTO @order_date, @user_id, @status_id
     FROM json_table(json_pay_for_order, '$'
                     COLUMNS (
-                        order_date   DATE         PATH '$.order.date',
-                        order_status VARCHAR(20)  PATH '$.order.orderStatus.status',
-                        user_email   VARCHAR(254) PATH '$.order.user.email'
+                        order_date DATE PATH '$.order.date',
+                        order_status VARCHAR(20) PATH '$.order.orderStatus.status',
+                        user_email VARCHAR(254) PATH '$.order.user.email'
                         )) AS orders
              LEFT JOIN order_status ON orders.order_status = order_status.status
              LEFT JOIN user ON orders.user_email = user.email;
@@ -20,7 +20,8 @@ BEGIN
     SELECT order_id
     INTO @order_id
     FROM `order`
-    WHERE `order`.date = @order_date AND `order`.user_id = @user_id;
+    WHERE `order`.date = @order_date
+      AND `order`.user_id = @user_id;
 
     UPDATE `order`
     SET `order`.status_id = @status_id
@@ -30,25 +31,25 @@ BEGIN
     INTO @payment_date, @payment_method
     FROM json_table(json_pay_for_order, '$'
                     COLUMNS (
-                        payment_date   DATE        PATH '$.payment.date',
+                        payment_date DATE PATH '$.payment.date',
                         payment_method VARCHAR(30) PATH '$.payment.method'
                         )) AS payments;
 
     INSERT INTO payment(order_id, date, method)
         VALUE (@order_id, @payment_date, @payment_method);
 
-    SELECT LAST_INSERT_ID() FROM payment INTO @payment_id;
+    SELECT LAST_INSERT_ID() INTO @payment_id;
 
     SELECT number, start_date, card_type_id
     INTO @number, @start_date, @card_type_id
     FROM json_table(json_pay_for_order, '$'
                     COLUMNS (
-                        number         VARCHAR(20) PATH '$.card.number',
-                        start_date     DATE        PATH '$.card.startDate',
+                        number VARCHAR(20) PATH '$.card.number',
+                        start_date DATE PATH '$.card.startDate',
                         card_type_name VARCHAR(30) PATH '$.card.cardType.type'
-                        )) AS card
-             LEFT JOIN card_type ON card.card_type_name = card_type.type;
+                        )) AS paid_card
+             LEFT JOIN card_type ON paid_card.card_type_name = card_type.type;
 
     INSERT INTO card(number, payment_id, start_date, card_type_id)
-    VALUES (@number, @payment_id, @start_date, @card_type_id);
+        VALUE (@number, @payment_id, @start_date, @card_type_id);
 END$$
