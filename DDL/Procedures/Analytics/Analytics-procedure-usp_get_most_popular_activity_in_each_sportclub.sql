579213a41fbdcb4df7642bcb5850f74112c2fb95
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2022-09-27 18:45:40 +0200

    fixed

--- DDL/Procedures/Analytics/Analytics-procedure-usp_get_most_popular_activity_in_each_sportclub.sql
+++ DDL/Procedures/Analytics/Analytics-procedure-usp_get_most_popular_activity_in_each_sportclub.sql
@@ -6,15 +6,19 @@ DROP PROCEDURE IF EXISTS usp_get_most_popular_activity_in_each_sportclub;
 
 CREATE PROCEDURE usp_get_most_popular_activity_in_each_sportclub()
 BEGIN
-    SELECT sportclub_name, activity_name most_popular_activity, MAX(visits_number) visits
-    FROM (SELECT s.name sportclub_name, a.name activity_name, COUNT(a.name) visits_number
-          FROM visit v
-                   LEFT JOIN sportclub_activity sa
-                             on v.sportclub_activity_id = sa.sportclub_activity_id
+    SELECT         sportclub_name,
+                   activity_name most_popular_activity,
+                   MAX(visits_number) visits
+    FROM   (SELECT s.name sportclub_name,
+                   a.name activity_name,
+                   COUNT(a.name) visits_number
+            FROM visit v
+                   LEFT JOIN sportclub_activity sa ON v.sportclub_activity_id = sa.sportclub_activity_id
                    LEFT JOIN sportclub s ON sa.sportclub_id = s.sportclub_id
                    LEFT JOIN activity a ON sa.activity_id = a.activity_id
-          GROUP BY s.name, a.name
-          ORDER BY visits_number DESC) AS visits
+            GROUP BY s.name, a.name
+            ORDER BY visits_number DESC
+            ) AS visits
     GROUP BY sportclub_name
     ORDER BY sportclub_name;
 END$$;
--- DDL/Procedures/JSONParser/Procedure-usp_parse_json_visit.sql
+++ DDL/Procedures/JSONParser/Procedure-usp_parse_json_visit.sql
@@ -18,20 +18,20 @@ BEGIN
                       card_type_id
          FROM json_table(json_visit, '$'
                     COLUMNS (
-                             card_number VARCHAR(20) PATH '$.card.number',
+                             card_number     VARCHAR(20)  PATH '$.card.number',
                              sportclub_email VARCHAR(100) PATH '$.sportClub.email',
-                             activityName VARCHAR(30) PATH '$.activity.name',
-                             date_time DATETIME PATH '$.visit.dateTime'
+                             activityName    VARCHAR(30)  PATH '$.activity.name',
+                             date_time       DATETIME     PATH '$.visit.dateTime'
                              )
                     ) AS user_visit
              JOIN sportclub ON user_visit.sportclub_email = sportclub.email
              JOIN activity ON user_visit.activityName = activity.name
              JOIN card ON user_visit.card_number = card.number)
 
-    SELECT             card_id,
-                       sportclub_activity_id,
-                       date_time
-    FROM               cte_user_visit
+    SELECT            card_id,
+                      sportclub_activity_id,
+                      date_time
+    FROM              cte_user_visit
              JOIN sportclub_activity ON cte_user_visit.sportclub_id = sportclub_activity.sportclub_id
                 AND cte_user_visit.activity_id = sportclub_activity.activity_id
              JOIN sportclub_card_types ON cte_user_visit.sportclub_id = sportclub_card_types.sportclub_id
