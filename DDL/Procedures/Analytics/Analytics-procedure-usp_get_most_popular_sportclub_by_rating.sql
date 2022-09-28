e7270d21e5743582da010f715b7cab7a98a50d73
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2022-09-28 13:12:56 +0200

    fixed

--- DDL/Procedures/Analytics/Analytics-procedure-usp_get_most_popular_sportclub_by_rating.sql
+++ DDL/Procedures/Analytics/Analytics-procedure-usp_get_most_popular_sportclub_by_rating.sql
@@ -7,14 +7,13 @@ DROP PROCEDURE IF EXISTS usp_get_most_popular_sportclub_by_rating;
 CREATE PROCEDURE usp_get_most_popular_sportclub_by_rating(IN club_count TINYINT)
 BEGIN
 
-    SELECT concat(name, ', ', city, ', ', street, ', ', building) as `name`,
+    SELECT concat(name, ', ', city, ', ', street, ', ', building) as `sportclub name`,
            avg(rating.rate) AS 'average sportclub rate',
            count(rating.rate) AS 'rate count'
     FROM rating
              JOIN sportclub ON rating.sportclub_id = sportclub.sportclub_id
     GROUP BY `name`
-    ORDER BY `rate count` DESC
+    ORDER BY `average sportclub rate` DESC
     LIMIT club_count;
-
 
 END$$;
